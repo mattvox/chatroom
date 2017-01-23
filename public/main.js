57a6ef7b54cf85030c74aa578f4de78b67ae5fe1
mattvox <vox-mini@Matthews-Mini.home>
2017-01-22 20:47:34 -0500

    Added notifications for when a client connects/disconnects and a count showing how many users are connected.

--- public/main.js
+++ public/main.js
@@ -21,3 +21,14 @@ $(document).ready(function() {
     socket.on('message', addMessage);
 });
 
+//Broadcast and display a message to connected users when someone connects or disconnects
+
+//Display a count of how many users are connected
+
+//Add support for nicknames
+
+//Add "{user} is typing" functionality
+
+//Show who's online
+
+//Add private messaging
--- server.js
+++ server.js
@@ -9,14 +9,36 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 
+var userCount = 0;
+
 io.on('connection', function(socket) {
+    userCount++;
     console.log('Client connected');
+    socket.emit('message', 'You have connected');
+    socket.broadcast.emit('message', 'New client connected');
+    io.emit('message', userCount + ' users connected')
     
     socket.on('message', function(message) {
         console.log('Received message:', message);
         socket.broadcast.emit('message', message);
     });
+    
+    socket.on('disconnect', function() {
+        userCount--;
+        console.log('Client disconnected')
+        socket.broadcast.emit('message', 'Client disconnected');
+    });
 })
 
 
+
+
 server.listen(process.env.PORT || 8080);
+
+//Add support for nicknames
+
+//Add "{user} is typing" functionality
+
+//Show who's online
+
+//Add private messaging
